7acf3ee9366b77f4c3e9112b411856429a0ecb0e
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2022-06-17 15:25:31 -0400

    Added and configured TypeGen

---
+++ Chatter/TypeGen/ChatterGenerationSpec.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using TypeGen.Core.SpecGeneration;
+
+namespace Chatter.TypeGen
+{
+    public class ChatterGenerationSpec : GenerationSpec
+    {
+        private const string OutputDir = "../ChatterApp/generated_types";
+
+        public override void OnBeforeGeneration(OnBeforeGenerationArgs args)
+        {
+            var allModelTypes = GetAllClassesInNameSpace("Chatter.Data.Models");
+            foreach (var modelType in allModelTypes)
+            {
+                AddClass(modelType, OutputDir);
+            }
+        }
+
+        private static List<Type> GetAllClassesInNameSpace(string nameSpace)
+        {
+            return Assembly.GetExecutingAssembly()
+                .GetExportedTypes()
+                .Where(t => t.IsClass && t.Namespace == nameSpace)
+                .ToList();
+        }
+    }
+}
