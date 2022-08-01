46fdb91e2da23af0237b870e9a38d04f444e5515
Adi Thakur <55326963+AdiThakur@users.noreply.github.com>
2022-08-01 12:41:12 -0400

    Added missing redirect in UnAuthGuard

--- ChatterApp/src/app/guards/un-auth.guard.ts
+++ ChatterApp/src/app/guards/un-auth.guard.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from "../services/auth.service";
+import { AbsolutePath } from "../routing/absolute-paths";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class UnAuthGuard implements CanActivate {
 
-	constructor(private authService: AuthService) {}
+	constructor(
+		private router: Router,
+		private authService: AuthService
+	) {}
+
 	canActivate(
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 		if (this.authService.hasUserSeenLogin) {
+			this.router.navigate([AbsolutePath.Home]);
 			return false;
 		} else {
 			return true;
